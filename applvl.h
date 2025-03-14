ce035a8e7083a9f5d5fdddad8468d8b5321dc37c
Toshio Koike <toshiokoike1970@gmail.com>
2025-03-14 23:42:35 +0900

    files added

---
+++ applvl.h
@@ -0,0 +1,32 @@
+/**************************************************************************
+ *   applvl.h                                                             *
+ *                                                                        *
+ *   Copyright (C) 2025-2025 PCR                                          *
+ *                                                                        *
+ *   This program is free software; you can redistribute it and/or modify *
+ *   it under the terms of the GNU General Public License as published by *
+ *   the Free Software Foundation; either version 2, or (at your option)  *
+ *   any later version.                                                   *
+ *                                                                        *
+ *   This program is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ *   GNU General Public License for more details.                         *
+ *                                                                        *
+ *   You should have received a copy of the GNU General Public License    *
+ *   along with this program; if not, write to the Free Software          *
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.            *
+ *                                                                        *
+ **************************************************************************/
+
+#ifndef applvl_h
+#define applvl_h
+
+#define APP_REL_LVL 0	// Application Release Level (0: experimental, 1: test, 2: stable)
+#define APP_REL_LVL_EXPERIMENTAL	0	// experimental release
+#define APP_REL_LVL_TEST			1	// test         release
+#define APP_REL_LVL_STABLE			2	// stable       release
+
+#endif // applvl_h
+
+// End of applvl.h
