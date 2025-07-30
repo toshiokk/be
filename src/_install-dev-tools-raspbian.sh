8311dd71ab752f25971abb4009633edc45110101
Toshio Koike <toshiokoike1970@gmail.com>
2025-07-30 22:11:54 +0900

    delete unused files

--- src/_install-dev-tools-raspbian.sh
+++
@@ -1,6 +0,0 @@
-echo "Installing packages needed for building BE editor on raspbian ..."
-sudo apt-get update
-sudo apt-get install libncursesw5-dev
-sudo apt-get install libpcre3-dev
-sudo apt-get install nkf
-sudo apt-get install libtool
